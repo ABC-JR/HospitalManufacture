9a80808ca611b53b27464be2555aebf25bdaf4ff
ABC <ertensabakioo@gmail.com>
2025-03-25 21:51:38 +0500

    temp 1

---
+++ app/src/main/java/com/example/hospitalinfrastructuremanagement/screens/MainApp.kt
@@ -0,0 +1,143 @@
+package com.example.hospitalinfrastructuremanagement.screens
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import coil3.compose.AsyncImage
+import com.example.hospitalinfrastructuremanagement.screens.mainpage.MainScreen
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainApp() {
+    val navconroller = rememberNavController()
+    val navBackStackEntry by navconroller.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+    val showBars = currentRoute != "signin"
+
+
+    Scaffold(
+        topBar ={
+            if(showBars){
+                MyTopAppBar()
+            }
+
+        } ,
+        bottomBar = {
+            if(showBars){
+                BottomBar(navconroller)
+            }
+
+        }
+    )
+    {
+        padding->
+
+        var isUserLoggedIn = false
+
+        val destination =
+            if(isUserLoggedIn){
+                "signin"
+                isUserLoggedIn = true
+            }
+        else{
+            "mainpage"
+        }
+        NavHost(navconroller , startDestination = destination) {
+            composable("signin") {
+
+            }
+            composable("mainpage") {
+                MainScreen(padding ,  navconroller)
+            }
+            composable("tasks") {
+
+            }
+            composable("profile") {
+
+            }
+        }
+
+    }
+}
+
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopAppBar() {
+    TopAppBar(
+        title = { Text("Hospital") },
+        navigationIcon = {
+            AsyncImage(
+                model = "https://i.pinimg.com/1200x/2e/02/bf/2e02bf2de46b5cbddf3cfbc16e83e822.jpg",
+                contentDescription = "logo",
+                modifier = Modifier.size(50.dp)
+            )
+        }
+    )
+}
+
+
+
+
+data class triple(
+    val name:String,
+    val route:String,
+    val id: ImageVector
+)
+
+@Composable
+fun BottomBar(navconroller : NavController){
+    val list = listOf(
+
+        triple("Hospital" , "mainpage" , Icons.Default.Add) ,
+        triple("Tasks" , "tasks" ,Icons.Default.List ) ,
+        triple("Profile" , "profile" , Icons.Default.Person)
+    )
+
+    var selectedid by remember{ mutableStateOf(0)}
+
+    NavigationBar{
+        list.forEachIndexed{
+            index , item->
+            NavigationBarItem(
+                selected = selectedid ==index ,
+                onClick = {
+                    selectedid = index
+                    navconroller.navigate(item)
+                } ,
+                icon = {
+                    Icon(item.id , "")
+                } ,
+                label = {
+                    Text(text =item.name)
+                }
+            )
+        }
+    }
+}
